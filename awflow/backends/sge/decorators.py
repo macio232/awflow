31d5ca28c7364bccc9729dcb2944b4388cdd9e86
Maciej Falkiewicz <maciej.falkiewicz@hesge.ch>
2023-03-15 12:43:18 +0100

    Apply suggestions

--- awflow/backends/sge/decorators.py
+++ awflow/backends/sge/decorators.py
@@ -16,17 +16,14 @@ def cpus(node: Node, n: int) -> None:
 
 
 def memory(node: Node, memory: str, n: int = 1) -> None:
-    mem = float(re.match(r"\d+", memory).group(0)) / n
-    node['-l h_vmem='] = f'{mem:.2f}G'
+    pass
 
 
 def gpus(node: Node, n: int, memory: str=None) -> None:
     if n != 0:
-        node['-jc'] = os.environ.get('SGE_JOB_CLASS', 'gpu-container_g1.24h')
+        node['-jc'] = os.environ.get('SGE_GPU_JOB_CLASS', 'gpu-container_g1.24h')
         node['-ac'] = f"d={os.environ.get('SGE_BASE_CONTAINER', 'nvcr-cuda-11.7.0-ubuntu20.04')}"
-    else:
-        node['-jc'] = "pcc-normal"
 
 
 def timelimit(node: Node, timelimit: str) -> None:
-    node['-l h_rt='] = str(timelimit)
+    pass
